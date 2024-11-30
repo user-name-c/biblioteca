752ba5d00ab9490c939ee945f9b91abc6934cb7b
user-name-c <ivandgr_16@hotmail.com>
2024-11-29 18:43:26 -0600

    Initial setup: localhost working properly

---
+++ src/main/java/com/chomiko/biblioteca/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.chomiko.biblioteca.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    public String helloWorld(){
+        String mensaje = "Sin problema en la configuracion inicial";
+        System.out.println(mensaje);
+        return mensaje;
+    }
+}
